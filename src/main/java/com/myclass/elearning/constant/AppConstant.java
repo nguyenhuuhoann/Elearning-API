67e33fb90904eebb3a53c9fd9c04690a31ec8817
NguyenHuuHoan <nguyenhuuhoann99@gmail.com>
2021-05-24 19:31:45 +0700

    add constant for pageable

---
+++ src/main/java/com/myclass/elearning/constant/AppConstant.java
@@ -0,0 +1,9 @@
+package com.myclass.elearning.constant;
+
+public class AppConstant {
+
+    public static final String DEFAULT_PAGE = "0";
+
+    public static final String DEFAULT_SIZE = "5";
+
+}
